545ca07b38402a122d6faf70e39b45dac816a2ef
doCent90 <78255120+doCent90@users.noreply.github.com>
2021-08-28 11:51:19 +0300

    code has been reworked

---
+++ Assets/Group1/Scripts/KillEnemys.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class KillEnemys : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private GameObject _blackScreen;    
+    [SerializeField] private EnemyMover[] _enemies;
+
+    private const float Distance = 0.2f;
+
+    public void ShowBlackScreen()
+    {
+        _blackScreen.SetActive(true);
+    }
+
+    private void Update()
+    {
+        foreach (var enemy in _enemies)
+        {
+            if (enemy == null)
+                continue;
+
+            if (Vector3.Distance(_player.transform.position, enemy.transform.position) < Distance)
+            {
+                _player.InitializeCollision(enemy);
+            }
+
+        }
+    }
+}
